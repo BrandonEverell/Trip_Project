e8022b640af0d6d41b415f0e3e8747bb431c705c
Brandon Everell <brandoneverell@hotmail.com>
2018-01-08 14:28:38 -0800

    optimizing for heroku

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+googleClientID: process.env.googleClientID,
+googleClientSecret: process.env.googleClientSecret,
+FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID,
+FACEBOOK_APP_SECRET: process.env.FACEBOOK_APP_SECRET
+};
